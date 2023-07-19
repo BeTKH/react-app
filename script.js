fe09166981e498b57106649049c7b33318256c75
Bekalue Tadesse <bekpostgrad@gmail.com>
2023-07-18 21:56:04 -0500

    moved files

--- script.js
+++
@@ -1,9 +0,0 @@
-function processForm() {
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var message = document.getElementById("message").value;
-    google.script.run.processForm({name: name, email: email, message: message});
-    alert("Your message has been sent!");
-    document.getElementById("name").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("message").value = "";
