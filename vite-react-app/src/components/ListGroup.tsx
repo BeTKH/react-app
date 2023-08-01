4d3633ab31603a912b86a4a403e3670d929d4a93
Bekalu <bekpostgrad@gmail.com>
2023-08-01 11:35:00 -0500

    react fragments - group elements

---
+++ vite-react-app/src/components/ListGroup.tsx
@@ -0,0 +1,19 @@
+function ListGroup(){
+
+    return (
+
+    
+        <>  
+            <h1>Lists</h1>
+            <ul className="list-group">
+                <li className="list-group-item">An item</li>
+                <li className="list-group-item">A second item</li>
+                <li className="list-group-item">A third item</li>
+                <li className="list-group-item">A fourth item</li>
+                <li className="list-group-item">a fifth item</li>
+            </ul>
+        </>
+    );
+}
+
+export default ListGroup;
