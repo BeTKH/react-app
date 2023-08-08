5eab8a8e36538cf6ef628ca9c9dd3ff475f2e422
Bekalu <bekpostgrad@gmail.com>
2023-08-07 21:22:54 -0500

    update msg

--- vite-react-app/src/Message.tsx
+++ vite-react-app/src/Message.tsx
@@ -1,11 +1,7 @@
-
-// PascalCasing 
-function Message(){
-    
-
-    // JSX: JavaScript XML
-    return <h1>Hello React!</h1>;
-
+// PascalCasing
+function Message() {
+  // JSX: JavaScript XML
+  return <h1>Hello Reac!</h1>;
 }
 // export it to be able to use this new component
 export default Message;
